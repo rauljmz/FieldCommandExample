4dc1fc14fc3af9d79f3e365fe0febdb60245a2e5
rauljmz <rauljmz@gmail.com>
2013-06-18 08:21:27 +0100

    initial

---
+++ FieldCommandPOC/Commands/Open.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore;
+using Sitecore.Web.UI.Sheer;
+
+namespace FieldCommandPOC.Commands
+{
+    using Sitecore.Shell.Framework.Commands;
+
+  
+    public class Open : Command
+    {
+        public override void Execute([NotNull] CommandContext context)
+        {
+            SheerResponse.Alert("open clicked", false);
+        }
+    }
+}
